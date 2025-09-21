b9185d283e5269780799373d3839a70702202d50
deminkmnn <148581675+deminkmnn@users.noreply.github.com>
2025-09-21 17:38:04 +0000

    Delete src directory

--- src/middleware/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-function errorMiddleware(err, req, res, next) {
-  console.error(err);
-  const status = err.status || 500;
-  res.status(status).json({ success: false, message: err.message || 'Internal Server Error' });
-}
-module.exports = errorMiddleware;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://${HOST}:${PORT}`);
-});
